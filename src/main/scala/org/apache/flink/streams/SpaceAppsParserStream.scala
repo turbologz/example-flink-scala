ba518f27e76a4ef51ef45b8a43e00acc621063e3
el-davo <david.ahern@emc.com>
2019-02-19 17:55:50 +0000

    Updated

--- src/main/scala/org/apache/flink/streams/SpaceAppsParserStream.scala
+++ src/main/scala/org/apache/flink/streams/SpaceAppsParserStream.scala
@@ -6,6 +6,7 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.cf.CloudFoundryLog
 import org.apache.flink.streaming.api.scala.DataStream
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction
 import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
@@ -18,15 +19,15 @@ class SpaceAppsParserStream {
         _.host.split('.')
       }
       .map(split => (split(1), split(2)))
-      .keyBy(0)
+      .keyBy("cloudFoundryInfo")
       .timeWindow(Time.of(10, TimeUnit.SECONDS))
       .apply(new UniqueWindowFunction())
   }
 }
 
-class UniqueWindowFunction() extends WindowFunction[String, String, Iterable[(String, String)], TimeWindow] {
+class UniqueWindowFunction() extends ProcessWindowFunction[Iterable[(String, String)], Collector[String], String, TimeWindow] {
 
-  override def apply(key: String, window: TimeWindow, input: Iterable[(String, String)], out: Collector[String]): Unit = {
+  override def apply(input: Iterable[(String, String)], out: Collector[String], key: String, window: TimeWindow): Unit = {
     val discount = input.map(t => t._2).toSet.size
     out.collect(s"Distinct elements: $discount")
   }
