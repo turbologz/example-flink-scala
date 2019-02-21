ba498e25d5d7fce62109087b785f8db0e474a151
el-davo <david.ahern@emc.com>
2019-02-21 08:52:33 +0000

    Refactoring

--- src/main/scala/org/apache/flink/CfSpacesAppsParser.scala
+++ src/main/scala/org/apache/flink/CfSpacesAppsParser.scala
@@ -1,11 +1,8 @@
 package org.apache.flink
 
-import java.util.Properties
-
-import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.scala._
+import org.apache.flink.sources.KafkaSource
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.streams.{CfLogParserStream, SpaceAppsParserStream}
 
 object CfSpacesAppsParser {
@@ -13,11 +10,7 @@ object CfSpacesAppsParser {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val properties = new Properties
-    properties.setProperty("bootstrap.servers", "prod-messaging-kafka:9092")
-    properties.setProperty("group.id", "cf-spaces-apps-parser")
-
-    val stream = env.addSource(new FlinkKafkaConsumer[String]("log-analysis", new SimpleStringSchema, properties))
+    val stream = env.addSource(new KafkaSource().getSource(env, "log-analysis", "cf-spaces-apps-parser"))
 
     new SpaceAppsParserStream()
       .parse(new CfLogParserStream().parse(stream))
---
+++ src/main/scala/org/apache/flink/sources/KafkaSource.scala
@@ -0,0 +1,18 @@
+package org.apache.flink.sources
+
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
+class KafkaSource {
+
+  def getSource(env: StreamExecutionEnvironment, topic: String, groupId: String): FlinkKafkaConsumer[String] = {
+    val properties = new Properties
+    properties.setProperty("bootstrap.servers", "prod-messaging-kafka:9092")
+    properties.setProperty("group.id", groupId)
+
+    new FlinkKafkaConsumer[String](topic, new SimpleStringSchema, properties)
+  }
+}
