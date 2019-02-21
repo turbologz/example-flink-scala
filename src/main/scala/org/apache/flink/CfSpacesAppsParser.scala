5098ec70611ce66188dc71f53db9f3f3c88842aa
el-davo <david.ahern@emc.com>
2019-02-21 08:39:38 +0000

    Updaed main stream

--- src/main/scala/org/apache/flink/CfSpacesAppsParser.scala
+++ src/main/scala/org/apache/flink/CfSpacesAppsParser.scala
@@ -1,17 +1,12 @@
 package org.apache.flink
 
 import java.util.Properties
-import java.util.concurrent.TimeUnit
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.scala._
-import org.apache.flink.cf.CloudFoundryLog
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.streams.{CfLogParserStream, SpaceAppsParserStream}
 
 object CfSpacesAppsParser {
 
@@ -24,16 +19,8 @@ object CfSpacesAppsParser {
 
     val stream = env.addSource(new FlinkKafkaConsumer[String]("log-analysis", new SimpleStringSchema, properties))
 
-    stream
-      .map((data: String) =>
-        (new ObjectMapper() with ScalaObjectMapper).registerModule(DefaultScalaModule).readValue(data, classOf[CloudFoundryLog])
-      )
-      .flatMap(cfLog => cfLog.host.split("."))
-      .map(word => word.toLowerCase)
-      .map(word => (word, 1))
-      .keyBy(0)
-      .timeWindow(Time.of(10, TimeUnit.SECONDS))
-      .sum(1)
+    new SpaceAppsParserStream()
+      .parse(new CfLogParserStream().parse(stream))
       .print()
 
     env.execute()
